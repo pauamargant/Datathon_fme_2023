98a7947355375c11632c7f0a9ee45bc4e343fc00
Pau Amargant <90143513+pauamargant@users.noreply.github.com>
2023-11-12 03:42:11 +0100

    visual

--- fashion.py
+++ fashion.py
@@ -2,6 +2,8 @@ from pytorch_metric_learning.distances import LpDistance
 import matplotlib.pyplot as plt
 import torch
 import streamlit as st
+import pickle
+import pandas as pd
 
 
 def display_images(paths):
@@ -15,10 +17,18 @@ def display_images(paths):
     plt.show()
 
 
-def get_image_path(cod_modelo_color, outfit_data):
+def get_image_path(cod_modelo_color, outfit_data, id=False):
+    if id:
+        index_dict = pickle.load(open("dataset/dict_index_modelo.pickle", "rb"))
+        cod_modelo_color = index_dict[cod_modelo_color]
     return outfit_data[outfit_data["cod_modelo_color"] == cod_modelo_color][
         "des_filename"
-    ].values[0][9:]
+    ].values[0]
+
+
+def find_item(filtered, k=1):
+    # sample k rows from filtered
+    return filtered.sample(k)["cod_modelo_color"].values
 
 
 from pytorch_metric_learning.distances import LpDistance
@@ -28,6 +38,7 @@ def get_embedding(model, x):
     return model(torch.tensor(x.values, dtype=torch.float32, requires_grad=True))
 
 
+@st.cache
 def get_recommendation(model, x, item_id, k=10, embeddings=None):
     if embeddings is None:
         embeddings = get_embedding(model, x)
@@ -43,6 +54,56 @@ def get_recommendation(model, x, item_id, k=10, embeddings=None):
     return topk[:k]
 
 
-def find_item(filtered, k=1):
-    # sample k rows from filtered
-    return filtered.sample(k)["cod_modelo_color"].values
+def get_model():
+    import torch
+    import torch.nn as nn
+    import torch.optim as optim
+    from torch.utils.data import Dataset, DataLoader
+
+    class MyNN(nn.Module):
+        def __init__(self, input_size, hidden_size1, hidden_size2, output_size):
+            super().__init__()
+            self.fc1 = nn.Linear(input_size, hidden_size1)
+            self.relu1 = nn.ReLU()
+            self.fc2 = nn.Linear(hidden_size1, hidden_size2)
+            self.relu2 = nn.ReLU()
+            self.fc3 = nn.Linear(hidden_size2, output_size)
+            self.relu3 = nn.ReLU()
+
+        def forward(self, x):
+            x = self.fc1(x)
+            x = self.relu1(x)
+            x = self.fc2(x)
+            x = self.relu2(x)
+            x = self.fc3(x)
+            x = self.relu3(x)
+            return x
+
+    x = pd.read_csv("x.csv")
+    input_size = x.shape[1]
+    hidden_size1 = 64
+    hidden_size2 = 32
+    output_size = 20
+
+    model = MyNN(input_size, hidden_size1, hidden_size2, output_size)
+    return model
+
+
+model = get_model()
+
+from restrictions import Outfit
+
+
+def get_outfit(base, product_data):
+    compatbile = False
+    while not compatbile:
+        # get random 3 other items
+        other_items = product_data.sample(3)
+        # to list of rows
+        other_items = [row for _, row in other_items.iterrows()]
+        # make outfit
+        outfit = Outfit([base] + other_items)
+        # check if outfit is
+        if outfit.get_error() == 0:
+            compatbile = True
+    return outfit
--- restrictions.py
+++ restrictions.py
@@ -2,96 +2,140 @@ import networkx as nx
 
 G = nx.Graph()
 
-G.add_node('START', pos=(0, 0))
-G.add_node('TOP', pos=(1, 0))
-G.add_node('BOTTOM', pos=(2, 0))
-G.add_node('FOOTWEAR', pos=(3, 0))
-G.add_node('ACCESSORIES', pos=(4, 0))
-G.add_node('COMPLETE_SET', pos=(5, 0))
-G.add_node('END', pos=(6, 0))
-G.add_node('ERROR', pos=(7, 0))
+G.add_node("START", pos=(0, 0))
+G.add_node("TOP", pos=(1, 0))
+G.add_node("BOTTOM", pos=(2, 0))
+G.add_node("FOOTWEAR", pos=(3, 0))
+G.add_node("ACCESSORIES", pos=(4, 0))
+G.add_node("COMPLETE_SET", pos=(5, 0))
+G.add_node("END", pos=(6, 0))
+G.add_node("ERROR", pos=(7, 0))
 
 # START = ( top -> B | completeSet -> F)
-G.add_edge('START', 'TOP', weight=0)
-G.add_edge('START', 'COMPLETE_SET', weight=0)
+G.add_edge("START", "TOP", weight=0)
+G.add_edge("START", "COMPLETE_SET", weight=0)
 
 # B = (bottom -> C)
-G.add_edge('TOP', 'BOTTOM', weight=0)
+G.add_edge("TOP", "BOTTOM", weight=0)
 
 # C = (footwear -> D)
-G.add_edge('BOTTOM', 'FOOTWEAR', weight=0)
+G.add_edge("BOTTOM", "FOOTWEAR", weight=0)
 
 # D = (accessories -> E) | (end -> END)
-G.add_edge('FOOTWEAR', 'ACCESSORIES', weight=0)
-G.add_edge('FOOTWEAR', 'END', weight=0)
+G.add_edge("FOOTWEAR", "ACCESSORIES", weight=0)
+G.add_edge("FOOTWEAR", "END", weight=0)
 
 # E = (end -> END)
-G.add_edge('ACCESSORIES', 'END', weight=0)
+G.add_edge("ACCESSORIES", "END", weight=0)
 
 # F = (footwear -> D)
-G.add_edge('COMPLETE_SET', 'FOOTWEAR', weight=0)
+G.add_edge("COMPLETE_SET", "FOOTWEAR", weight=0)
 
 # connect each state to the ERROR state
-G.add_edge('START', 'ERROR', weight=-100)# it menas not possible classes
-G.add_edge('TOP', 'ERROR', weight=-1) 
-G.add_edge('BOTTOM', 'ERROR', weight=-0.66)
-G.add_edge('FOOTWEAR', 'ERROR', weight=-0.33)  
-G.add_edge('ACCESSORIES', 'ERROR', weight=-100)
-G.add_edge('COMPLETE_SET', 'ERROR', weight=-1)
+G.add_edge("START", "ERROR", weight=-100)  # it menas not possible classes
+G.add_edge("TOP", "ERROR", weight=-1)
+G.add_edge("BOTTOM", "ERROR", weight=-0.66)
+G.add_edge("FOOTWEAR", "ERROR", weight=-0.33)
+G.add_edge("ACCESSORIES", "ERROR", weight=-100)
+G.add_edge("COMPLETE_SET", "ERROR", weight=-1)
 
-def error_function(G=G, state='START', actions_list=[], error_value=0):
-    
-    if state == 'END' and len(actions_list) == 0:
+
+def error_function(G=G, state="START", actions_list=[], error_value=0):
+    if state == "END" and len(actions_list) == 0:
         if error_value > 0:
             return error_value
         else:
             return 0
-    
-    if state == 'ERROR':
+
+    if state == "ERROR":
         return error_value
-    
+
     action = actions_list.pop(0)
 
-    if action in G[state] and action != 'ERROR' and G[state][action]:
+    if action in G[state] and action != "ERROR" and G[state][action]:
         return error_function(G, action, actions_list, error_value)
-    elif action in G[state] and action != 'ERROR' and not G[state][action]:
-        return error_function(G, action, actions_list, error_value + G[state]['ERROR']['weight'])
-    else:     
-        return error_function(G, 'ERROR', [], error_value + G[state]['ERROR']['weight'])
+    elif action in G[state] and action != "ERROR" and not G[state][action]:
+        return error_function(
+            G, action, actions_list, error_value + G[state]["ERROR"]["weight"]
+        )
+    else:
+        return error_function(G, "ERROR", [], error_value + G[state]["ERROR"]["weight"])
 
 
-class Outfit():
-    
-    def __init__(self, rows=[]):
+class Outfit:
+    def __init__(self, items):
+        self.items = items
 
+    def get_restrictions(self):
+        actions_list = ["START"]
+        for item in self.items:
+            if item == "TOP":
+                actions_list.append("TOP")
+            elif item == "BOTTOM":
+                actions_list.append("BOTTOM")
+            elif item == "FOOTWEAR":
+                actions_list.append("FOOTWEAR")
+            elif item == "ACCESSORIES":
+                actions_list.append("ACCESSORIES")
+            elif item == "COMPLETE_SET":
+                actions_list.append("COMPLETE_SET")
+        actions_list.append("END")
+        return error_function(G, "START", actions_list, 0)
+
+    def __init__(self, rows=None):
+        """
+        Initializes an Outfit object with the given rows.
+
+        Args:
+        - rows: a list of rows representing the outfit components.
+        """
+        if rows is None:
+            rows = []
+        self.top = False
+        self.bottom = False
+        self.accessories = False
+        self.complete_set = False
+        self.footwear = False
         for row in rows:
-            if row[row["des_product_family"] == "Footwear"]:
+            if row["des_product_family"] == "Footwear":
                 self.footwear = True
             else:
-                if row[row["des_product_category"] == "Top"]:
+                if row["des_product_category"] == "Top":
                     self.top = True
-                if row[row["des_product_category"] == "Bottom"]:
+                if row["des_product_category"] == "Bottom":
                     self.bottom = True
-                if row[row["des_product_category"] == "Accesories, Swim and Intimate"]:
-                    self.accessores = True
-                if row[row["des_product_category"] == "Dresses, jumpsuits and Complete set"]:
-                    self.Complete_set = True
+                if row["des_product_category"] == "Accesories, Swim and Intimate":
+                    self.accessories = True
+                if row["des_product_category"] == "Dresses, jumpsuits and Complete set":
+                    self.complete_set = True
 
     def get_error(self):
+        """
+        Returns the error value of the outfit.
+
+        Returns:
+        - The error value of the outfit.
+        """
         actions_list = self.get_actions_list()
         return error_function(actions_list)
-    
+
     def get_actions_list(self):
+        """
+        Returns a list of actions based on the outfit components.
+
+        Returns:
+        - A list of actions based on the outfit components.
+        """
         actions_list = []
         if self.top:
             actions_list.append("TOP")
         if self.bottom:
             actions_list.append("BOTTOM")
-        if self.Complete_set:
+        if self.complete_set:
             actions_list.append("COMPLETE SET")
         if self.footwear:
             actions_list.append("FOOTWEAR")
-        if self.accessores:
+        if self.accessories:
             actions_list.append("ACCESSORIES")
-        
+
         return actions_list
